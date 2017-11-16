f4b9c35141e258674ad24195dca64ec087e4b724
Gerard Kabre <gerard.kabre@gmail.com>
2017-11-16 19:17:03 +0100

    Added comments to the module

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//************** DOM Elements Object **************//
 (function(){
         var app = {
             init: function () { 
@@ -29,11 +28,13 @@
                 });
                 this.button.addEventListener("click", this.mergeWords.bind(this)); 
             },
+            // When 1 checkbox clicked, uncheck the others. 
             uncheckOtherBoxes: function() {
                 this.checkBoxes.forEach((checkbox) => { 
                      checkbox !== event.target ? checkbox.checked = false : true;  
                 });
             },
+            // Counter of all the combinations possible
             combinationsCounter: function() {
                 this.arrTextArea1 = this.textArea1.value.split('\n'); 
                 this.arrTextArea2 = this.textArea2.value.split('\n'); 
@@ -41,6 +42,7 @@
                 this.totalCombinations = this.arrTextArea1.length * this.arrTextArea2.length * this.arrTextArea3.length; 
                 this.displayCombinations.innerHTML = "<b>" + this.totalCombinations + "</b>" + " possible combinations"; //MOVE it to Render, or Dont, Speed and this thigns.
             },
+            // Create a new arraw with combined strings from the 3 arrays 
             mergeWords: function() {
                 this.mergedArr = []; 
                 this.arrTextArea1.map( x => {
@@ -54,11 +56,13 @@
                 this.optionChecked();
                 this.render()
             },
+            // Eliminate whitespaces created when one of the text areas is empty
             eliminateWhiteSpace: function() {
                 this.mergedArr.forEach((element, index, array) => {
                     array[index] = element.replace(/^\s+/g, "").replace(/\s+$/g, "").replace(/\s{2}/g, " "); 
                  }); 
             },
+            // What to render depending on the checkbox checked by the user. 
             optionChecked: function() { 
                 if(this.exactMatch.checked) {
                     this.mergedArr.forEach((element, index, array) => {
@@ -74,9 +78,11 @@
                     }); 
                 }
             },
+            // Default checkbox checked the Broad Match Type. 
             basicConfig: function(){
                 this.broadMatch.checked = true; 
             },
+            // first Re-set output to blank and then Create elements to the DOM.
             render: function() {
                     this.textOutput.textContent = ""; 
                     this.mergedArr.forEach(function(x) {   
@@ -86,7 +92,9 @@
                     }.bind(this))      
             },
         }
+
         app.init(); 
+
     })(); 
     
 
